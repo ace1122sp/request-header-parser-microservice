f974f96e534cf615a88cc9e4ae458c5ec2427267
ace1122sp <ace11.space22@gmail.com>
2018-01-14 20:07:37 +0100

    raw node

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const hostname = 'localhost';
+const port = 9000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  const obj = {
+    lang: req.headers['accept-language'],
+    user: req.headers['user-agent'],
+    ip: req.headers['x-forwarded-for']
+  }
+  // res.end(JSON.stringify(req.headers['x-forwarded-for'] || req.connection.remoteAddress));
+  res.end(JSON.stringify(obj));
+});
+
+server.listen(port, hostname => {
+  console.log('Server started at port ' + port + '...');
+});
