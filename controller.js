696dc21183cf5078f5f6005e341ad0e5fc41ddda
ace1122sp <ace11.space22@gmail.com>
2018-01-15 21:24:54 +0100

    fixed regex

--- controller.js
+++ controller.js
@@ -3,8 +3,8 @@ const getUser = (req, res) => {
   const regexOperatingSystem = /(?=\().*?(?=;)/; //some problem with regex
   let user;
 
-  const ip = regexLanguageAndIPAddress.exec(req.headers['x-forwarded-for']);
-  const lang = regexLanguageAndIPAddress.exec(req.headers['accept-language']);
+  const ip = JSON.stringify(regexLanguageAndIPAddress.exec(req.headers['x-forwarded-for'])).slice(2, -2);
+  const lang = JSON.stringify(regexLanguageAndIPAddress.exec(req.headers['accept-language'])).slice(2, -2);
   let operatingSystem = JSON.stringify(regexOperatingSystem.exec(req.headers['user-agent'])).slice(3, -2);
 
   user = {ip, lang, operatingSystem};
